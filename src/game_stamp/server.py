55951924651e2fa283d497c8c525f2433b9d0d14
jc <1>
2025-06-11 22:15:13 +0800

    update image model

--- src/game_stamp/server.py
+++ src/game_stamp/server.py
@@ -1,96 +1,53 @@
-import json
-
-import requests
-from mcp.server.fastmcp import FastMCP
-import openai
-import os
+import replicate
 from dotenv import load_dotenv
+import os
 
-# Load environment variables from .env file
+from langchain_core.tools import tool
+import pathlib
+
 load_dotenv()
 
-# Initialize OpenAI client
-client = openai.OpenAI(base_url="http://38.246.112.171:3000/", api_key=os.getenv("OPENAI_API_KEY"))
 
-
-# mcp = FastMCP(
-#     "game_stamp",
-# )
-
-
-# @mcp.tool()
-def generate_image(description: str, output_dir: str, file_name: str) -> str:
+def generate_image(description: str, output_dir: str, file_name: str, aspect_ratio: str) -> str:
     """
-    Generate an image based on a description using GPT-4o Vision
-    Example usage: generate_image(description="A beautiful sunset over a calm ocean", output_dir="./output", file_name="sunset.png")
+    Generate a svg image based on a description using recraft-ai / recraft-v3-svg
+
+    Example usage:
+        generate_image(description="A beautiful sunset over a calm ocean", output_dir="./output", file_name="sunset")
+
+    Note: use SVG as follows; otherwise, a white background will appear instead of a transparent one
+        ```html
+        <embed src="./output/sunset.svg" width="300" height="100"
+        type="image/svg+xml"
+        pluginspage="http://www.adobe.com/svg/viewer/install/" />
+        ```
 
     :param description: The description of the image to generate.
     :param output_dir: The directory to save the generated image.
     :param file_name: The name of the generated image.
+    :param aspect_ratio: The aspect ratio of the generated image.
     :return: A message indicating the success or failure of the image generation.
     """
     try:
-        # Create a prompt that includes the image specifications
-        prompt = f"生成游戏素材，png，透明背景。 {description}"
-        url = f"{os.getenv('IMAGE_API_BASE')}/v1/chat/completions"
-
-        payload = json.dumps({
-            "stream": False,
-            "model": "gpt-4o-image-vip",
-            "messages": [
-                {
-                    "content": prompt,
-                    "role": "user"
-                }
-            ]
-        })
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {os.getenv("IMAGE_API_KEY")}'
+        input = {
+            "prompt": f"生成一个SVG格式的矢量图形，扁平化设计，简洁的线条，有限的颜色（不超过5种），清晰的轮廓，无渐变和阴影。图形描述：{description}。该图形将用于游戏素材。",
+            "aspect_ratio": aspect_ratio
         }
+        output = replicate.run(
+            "recraft-ai/recraft-v3-svg",
+            input=input
+        )
+        # 检查输出目录是否存在，如果不存在则创建
+        output_path = pathlib.Path(output_dir)
+        output_path.mkdir(parents=True, exist_ok=True)
 
-        response = requests.request("POST", url, headers=headers, data=payload)
-        lines = response.text.split('\\n')
-        line = ''
-        for line in lines:
-            if line.startswith('[点击下载]'):
-                break
-        url = line.replace('[点击下载]', '').strip("()")
-        print(f"image url:{url}")
-        download_image(url, output_dir, file_name)
-
-        return f"Image successfully generated and saved to {output_dir}"
+        with open(output_path.joinpath(f"{file_name}.svg"), "wb") as file:
+            file.write(output.read())
+            print(f"generate finish :{description}")
+            return f"Image successfully generated and saved to {os.path.join(output_dir, f'{file_name}.svg')}"
 
     except Exception as e:
         return f"Error generating image: {str(e)}"
 
 
-def download_image(url, save_path, new_name):
-    try:
-        # 发送 HTTP GET 请求获取图片
-        response = requests.get(url, stream=True)
-        response.raise_for_status()  # 检查请求是否成功
-
-        # 确保保存路径的目录存在
-        os.makedirs(save_path, exist_ok=True)
-
-        # 拼接新的文件路径和名称
-        file_extension = os.path.splitext(url)[1]  # 获取原始文件的扩展名
-        new_file_path = os.path.join(save_path, f"{new_name}{file_extension}")
-
-        # 写入文件
-        with open(new_file_path, 'wb') as file:
-            for chunk in response.iter_content(1024):
-                file.write(chunk)
-
-        print(f"图片已成功下载并保存为: {new_file_path}")
-        return new_file_path
-    except Exception as e:
-        print(f"下载图片时出错: {e}")
-        return None
-
-# if __name__ == "__main__":
-#     mcp.run()
-if __name__ == '__main__':
-    # download_image("https://filesystem.site/cdn/download/20250607/7VqeOZt2C0DI3QFZy4cYGz9ygPyVoM.png",'game_assets','start_button.png')
-    os.makedirs('game_assets', exist_ok=True)
+generate_image_tool = tool()(generate_image)
--- usage.py
+++ usage.py
@@ -1,16 +1,19 @@
+import inspect
 import os
 
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
+from langchain_core.tools import StructuredTool
 from langgraph_codeact import create_codeact
 from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.tools import tool as create_tool
 
-from src.game_stamp.server import generate_image
+from src.game_stamp.server import generate_image_tool
 
 import builtins
 import contextlib
 import io
-from typing import Any
+from typing import Any, Optional
 
 load_dotenv()
 
@@ -35,20 +38,51 @@ def eval(code: str, _locals: dict[str, Any]) -> tuple[str, dict[str, Any]]:
     new_vars = {key: _locals[key] for key in new_keys}
     return result, new_vars
 
+def create_default_prompt(tools: list[StructuredTool], base_prompt: Optional[str] = None):
+    """Create default prompt for the CodeAct agent."""
+    tools = [t if isinstance(t, StructuredTool) else create_tool(t) for t in tools]
+    prompt = f"{base_prompt}\n\n" if base_prompt else ""
+    prompt += """You are a professional HTML5 full-stack game development expert capable of generating directly runnable web games based on user requirements. You are proficient in the HTML/CSS/JavaScript trio, and well-versed in core technologies such as Canvas rendering, game loops, and event handling.
 
-tools = [generate_image]
+You will be given a task to perform. You should output either
+- ONE Python code snippet that provides the solution to the task, or a step towards the solution. Any output you want to extract from the code should be printed to the console. Code should be output in a fenced code block.
+- text to be shown directly to the user, if you want to ask for more information or provide the final answer.
+
+In addition to the Python Standard Library, you can use the following functions:
+"""
+
+    for tool in tools:
+        prompt += f'''
+def {tool.name}{str(inspect.signature(tool.func))}:
+    """{tool.description}"""
+    ...
+'''
+
+    prompt += """
+
+Variables defined at the top level of previous code snippets can be referenced in your code.
+
+Reminder: 
+-one step, one code snippet
+-use Python code snippets to call tools
+-execute actions step by step, pausing after each step to verify success and check if subsequent actions depend on previous results before proceeding. Always confirm dependencies and outcomes before continuing. 
+-proper use of the aforementioned tools can enhance the visual appeal and fun of the game, while maintaining a consistent style."""
+    return prompt
+
+
+tools = [generate_image_tool]
 model = ChatOpenAI(
     openai_api_key=os.getenv("OPENROUTER_API_KEY"),
     openai_api_base=os.getenv("OPENROUTER_BASE_URL"),
     model_name="anthropic/claude-sonnet-4",
 )
 
-code_act = create_codeact(model, tools, eval)
+code_act = create_codeact(model, tools, eval,prompt=create_default_prompt(tools))
 agent = code_act.compile(checkpointer=MemorySaver())
 
 messages = [{
     "role": "user",
-    "content": "生成一个html游戏，屏幕上有很多堆叠非常多层的方块，玩家需要点击屏幕上的方块，将其移至底部的7格槽位中，凑齐3个相同图案即可消除。若槽位被填满且无法消除，则游戏失败。请使用generate_image把开始按钮搞得炫酷一点，别的不要用图像素材，用js做就行。另外，一次输出做一件事情，不要一次输出一个以上的代码块"
+    "content": "生成一个html游戏，屏幕上有很多堆叠非常多层的方块，玩家需要点击屏幕上的方块，将其移至底部的7格槽位中，凑齐3个相同图案即可消除。若槽位被填满且无法消除，则游戏失败。"
 }]
 for typ, chunk in agent.stream(
         {"messages": messages},
